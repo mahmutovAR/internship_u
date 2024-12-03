52445832106c9b2941125465008e709befd56f16
Aidar_Mahmutov <armax045@gmail.com>
2024-12-03 14:21:14 +0400

    task:  U13 Basic Auth

--- pages/basic_auth_page.py
+++ pages/basic_auth_page.py
@@ -6,21 +6,15 @@ from . import BasePage
 
 
 class BasicAuthPage(BasePage):
-    def open_page(self) -> None:
+    def open_page(self, username: str, password: str) -> None:
         with allure.step('Открыть страницу'):
-            self.open_url(PageUrls.basic_auth_page)
+            domain = PageUrls.basic_auth_page.split('://')[1]
+            url = f'https://{username}:{password}@{domain}'
+            self.open_url(url)
 
     def click_display_image(self) -> None:
         with allure.step('Нажать "Display Image"'):
             self.click_element(BasicAuthLocators.display_image)
 
-    # def switch_to_iframe(self) -> None:
-    #     self.switch_to_iframe_by_locator(AlertsLocators.iframe)
-
-    def enter_login_and_password(self, login: str, password: str) -> None:
-        with allure.step('Ввести логин и пароль"'):
-            self.enter_login_and_password_to_alert(login, password)
-
-    def assert_authorization(self) -> None:
-        with allure.step('Проверить, что авторизация прошла успешно'):
-            pass
+    def auth_image_is_visible(self) -> bool:
+        return self.element_is_visible(BasicAuthLocators.auth_image)
--- tests/test_basic_auth.py
+++ tests/test_basic_auth.py
@@ -9,29 +9,31 @@ from pages import BasicAuthPage
 
 @allure.severity(severity_level.NORMAL)
 @allure.epic("Smoke тест")
-@allure.feature("Alert input")
+@allure.feature("Basic Auth")
 @allure.testcase("Задача U13")
-@allure.story("""Нажать "Display Image", ввести логин и пароль""")
-@allure.title("Ввод логина и пароля в всплывающем окно")
+@allure.story("""Открыть страницу, пройти базовую аутентификацию""")
+@allure.title("Базовая аутентификация")
 @allure.description(
     """
-    Цель: Проверить ввод логина и пароля в всплывающем окно
+    Цель: Проверить базовую аутентификацию
 
     Предусловие:
         - Открыть браузер
 
     Шаги:
-        1. Открыть страницу
+        1. Открыть страницу со встроенным username и password для базовой аутентификации
+        2. Проверить, что "Authenticated Image" не отображается
         2. Нажать "Display Image"
-        3. Ввести логин и пароль
-        4. Проверить, что авторизация прошла успешно
+        3. Проверить, что "Authenticated Image" не отображается
 
     Ожидаемый результат:
-        - После ввода в всплывающем окне логина и пароля, авторизация прошла успешно""")
-@pytest.mark.parametrize('login, password', [(BasicAuthData.login, BasicAuthData.password)])
-def test_alert_log_in(browser: fixture, login: str, password: str):
-    alerts_page = BasicAuthPage(browser)
-    alerts_page.open_page()
-    alerts_page.click_display_image()
-    alerts_page.enter_login_and_password(login, password)
-    alerts_page.assert_authorization()
+        - Базовая аутентификация прошла успешно""")
+@pytest.mark.parametrize('username, password', [(BasicAuthData.username, BasicAuthData.password)])
+def test_basic_auth(browser: fixture, username: str, password: str):
+    basic_auth = BasicAuthPage(browser)
+    basic_auth.open_page(username, password)
+    with allure.step('Проверить, что "Authenticated Image" не отображается'):
+        assert not basic_auth.auth_image_is_visible(), 'Authenticated Image expected not to be visible'
+    basic_auth.click_display_image()
+    with allure.step('Проверить, что "Authenticated Image" отображается'):
+        assert basic_auth.auth_image_is_visible(), 'Authenticated Image expected to be visible'
